c94aa6ac8ce5874a639e00e6f7a9a1167b015042
Michal Jamroz <mjamroz90@gmail.com>
2017-10-04 17:53:25 +0200

    Fix in resizing.

--- transformers/celeb.py
+++ transformers/celeb.py
@@ -11,7 +11,7 @@ class CelebDsTransformer(object):
     def get_img(self, img_path):
         img = cv2.imread(img_path)
         crop = img_ops.center_crop(img, self.crop_size, self.crop_size) if self.crop_size is not None else img
-        resized_img = cv2.resize(crop, self.out_size) if self.out_size is not None else crop
+        resized_img = cv2.resize(crop, (self.out_size, self.out_size)) if self.out_size is not None else crop
         return img_ops.bound_image_values(resized_img)
 
     def img_size(self):
