a126044191e627daf4d4b780c4853aac572febd7
Michal Jamroz <mjamroz90@gmail.com>
2017-10-11 11:33:46 +0200

    Removing learning rate.

--- trainers/sup_cnn_trainer.py
+++ trainers/sup_cnn_trainer.py
@@ -55,8 +55,8 @@ class SupCnnTrainer(object):
                                                                                      self.labels: train_labels})
 
                 current_step = global_step.eval(sess)
-                self.logger.info("Epoch: %d/%d, batch: %d/%d, Class. loss: %.8f, global step: %d, lr: %s" % (epoch,
-                    epochs_num, batch_counter, batches_num, softmax_loss_val, current_step, str(sess.run(lr))))
+                self.logger.info("Epoch: %d/%d, batch: %d/%d, Class. loss: %.8f, global step: %d" % (epoch,
+                    epochs_num, batch_counter, batches_num, softmax_loss_val, current_step))
 
                 if current_step % testing_interval == 0:
                     self.__test_cnn(sess, test_model_out_layer)
