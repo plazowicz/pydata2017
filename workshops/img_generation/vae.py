b0cd81717c1e9144c1fb96badea81c6f1f5dfc45
Michal Jamroz <mjamroz90@gmail.com>
2017-10-17 17:08:44 +0200

    Added VAE generator code.

--- workshops/img_generation/vae.py
+++ workshops/img_generation/vae.py
@@ -1,4 +1,3 @@
-import os.path as op
 import tensorflow as tf
 
 from utils import device_utils
@@ -10,17 +9,28 @@ from utils.logger import log
 @log
 class VaeImgGenerator(object):
 
+    # load_gen_with_weights_func(sess, latent_vector, saved_parameters_path)
     def __init__(self, gen_params_path, load_gen_with_weights_func,
                  get_latent_dim_func=net_utils.read_latent_dim_from_gen_weights):
-        # TODO - get latent dimensionality
-        pass
+
+        self.gen_params_path = gen_params_path
+        self.load_gen_with_weights_func = load_gen_with_weights_func
+        self.z_dim = get_latent_dim_func(self.gen_params_path)
 
     def generate_images(self, samples_num, out_path, transformer=lambda x: img_ops.unbound_image_values(x)):
         # TODO - instantiate tf Session
         # TODO - sample z randomly
-        z = None
+        sess = tf.InteractiveSession()
+        with tf.device(device_utils.get_device()):
+            z = tf.random_uniform(shape=[1, self.z_dim], minval=-1, maxval=1, dtype=tf.float32)
+            gen_out_layer, _ = self.load_gen_with_weights_func(sess, z, self.gen_params_path)
+            x_generated = gen_out_layer.outputs
+
         gen_images = []
+        for i in xrange(samples_num):
+            x_gen = sess.run(x_generated)[0]
+            gen_images.append(x_gen)
 
-        # TODO - run loop and generate image
+            self.logger.info("Generated %d/%d image" % (i, samples_num))
+
         img_ops.save_gen_images(gen_images, out_path, transformer)
-
